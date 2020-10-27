69028590f69876d6d2ef0a76f2aa61616c3a80df
svenlivingdocs <sven@livingdocs.io>
2020-10-27 08:53:43 +0100

    chore(mock slides): Removed
    - Removed no longer wanted mock slides

--- js/script.js
+++ js/script.js
@@ -1,24 +1,6 @@
 const data = {
   "slides":[
     {
-      "flag":"Mehr 100 km/h zu schnell, illegale Strassenrennen und Gefährdung anderer Verkehrsteilnehmer",
-      "title":"Raserrennen und Tempo-Exzesse",
-      "imageUrl": "../assets/preview-content/teaser-image-1.jpg",
-      "QRlink": "https://www.bluewin.ch/de/news/schweiz/gefaengnis-fuer-zwei-raser-gefordert-451194.html"
-    },
-    {
-      "flag":"241 Kilogramm schwere Kirschtorte, die eine Zuger Confiserie Anfang Jahr gebacken hatte, ist offenbar die Grösste der Welt",
-      "title":"Riesen-Kirschtorte aus Zug bricht Rekord",
-      "imageUrl": "../assets/preview-content/teaser-image-2.jpg",
-      "QRlink": "https://www.bluewin.ch/de/news/vermischtes/riesen-kirschtorte-aus-zug-bricht-rekord-451135.html"
-    },
-    {
-      "flag":"Bei neuen Massenprotesten in Belarus gegen Staatschef Alexander Lukaschenko am Wochenende sind 280 Menschen festgenommen worden",
-      "title":"280 Festnahmen bei Protesten gegen Lukaschenko",
-      "imageUrl": "../assets/preview-content/teaser-image-3.jpg",
-      "QRlink": "https://www.bluewin.ch/de/news/international/280-festnahmen-in-belarus-bei-protesten-gegen-lukaschenko-451223.html"
-    },
-    {
       "flag":"Über die Umbenennung der Basler St. Jakobshalle in Roger-Federer-Arena gibt es keine Volksabstimmung",
       "title":"Zu wenige Basler wollen eine Roger-Federer-Arena",
       "imageUrl": "../assets/preview-content/teaser-image-4.jpg",
@@ -31,28 +13,10 @@ const data = {
       "QRlink": "https://www.blue.ch/de/sport"
     },
     {
-      "flag":"Britto fehlt dem FCZ mehrere Wochen",
-      "title":"Der FC Zürich muss mehrere Wochen auf Willie Britto verzichten",
-      "imageUrl": "../assets/preview-content/teaser-image-6.jpg",
-      "QRlink": "https://www.bluewin.ch/de/sport/fussball-schweiz/britto-fehlt-dem-fcz-mehrere-wochen-451115.html"
-    },
-    {
       "flag":"Erfahren Sie, welche 17 Kandidatinnen Alanz Herzen erobern wollen",
       "title":"«What to Watch» Es wird wieder viel geflirtet und der Fremdschäm-Faktor ist garantiert",
       "imageUrl": "../assets/preview-content/teaser-image-7.jpg",
       "QRlink": "https://www.bluewin.ch/de/entertainment/tv-film/danke-bachelor-endlich-macht-der-montag-wieder-sinn-451226.html"
-    },
-    {
-      "flag":"Momente, in denen Frau im Boden versinken möchte",
-      "title":"Wie kommt es dazu, dass wir manchmal am liebsten im Boden versinken würden?",
-      "imageUrl": "../assets/preview-content/teaser-image-8.jpg",
-      "QRlink": "https://www.bluewin.ch/de/leben/lifestyle/momente-in-denen-frau-im-boden-versinken-moechte-449435.html"
-    },
-    {
-      "flag":"Geht das wirklich? Wir haben es getestet",
-      "title":"Google erkennt Lieder durch Vorsummen",
-      "imageUrl": "../assets/preview-content/teaser-image-9.jpg",
-      "QRlink": "https://www.bluewin.ch/de/digital/google-erkennt-lieder-durch-vorsummen-451139.html"
     },
     {
       "flag":"Endgegner für Playstation 5?",
